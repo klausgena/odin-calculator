8a3ae9ec66351d349a3493e4c06c35342e12a3d0
Nicolas Severyns <contact@nicolasseveryns.be>
2023-08-30 20:18:45 +0200

    Create basic functionality: javascript, css and html.

---
+++ calculator.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+// const firstNumber = 0;
+// const secondNumber = 0;
+// const operator = "";
+
+function operate(operator, firstNumber, secondNumber) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+    }
+    return result;
+}
+
+// debug 
+
+console.log(operate("-", 10, 7));
