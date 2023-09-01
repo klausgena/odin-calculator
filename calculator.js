654d0cfa5008a8524d43d58f8aaa294d7aa21e26
Nicolas Severyns <n@svrns.com>
2023-09-01 18:47:30 +0100

    Implement most functionality. Only left = calculate result.

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function operate(operator, firstNumber, secondNumber) {
         case "-":
             result = subtract(firstNumber, secondNumber);
             break;
-        case "*":
+        case "×":
             result = multiply(firstNumber, secondNumber);
             break;
         case "/":
@@ -33,27 +33,30 @@ function operate(operator, firstNumber, secondNumber) {
 // Display functions
 
 function refreshDisplay (newValue) {
-    const display = document.getElementById("display");
-    const pDisplay = display.firstElementChild;
+    const pDisplay = document.getElementById("display").firstElementChild;
     pDisplay.textContent = newValue;
 }
 
-// Main functionality
-
-function getFirstNumber (event, operateObj) {
-    // if 1st entry
-    // TODO: check if nog operator and not 0 - strip zero from beginning
-    const value = event.target.id;
-    if (!operateObj.number1) {
-        operateObj.number1 = "";
+function convertOperator (operator) {
+    switch (operator) {
+        case "add":
+            operator = "+";
+            break;
+        case "subtract":
+            operator = "-";
+            break;
+        case "multiply":
+            operator = "×";
+            break;
+        case "divide":
+            operator = "÷";
+            break;
+        case "equals":
+            operator = "=";
+            break;
     }
-    operateObj.number1 = operateObj.number1 + value;
-    refreshDisplay(operateObj.number1);
+    return operator;
 }
-function getOperator(event, operateObj) {};
-function getSecondNumber(event, operateObj) {};
-function getResult(operateObj) {};
-
 // Event listener
 
 const buttons = document.querySelectorAll("button");
@@ -61,7 +64,48 @@ let operateObj = {};
 buttons.forEach(button => button.addEventListener(
     "click",
     function (e) {
-        // while e.target.id is a number
-        getFirstNumber(e, operateObj);
+        // TODO: what if operator first? Or double operator?
+        // as long as number1 does not exist, break, 
+        const inputValue = convertOperator(e.target.id);
+        console.table(operateObj);
+        let displayValue = "";
+        // get left term
+        if (!operateObj.number1 && /[0-9]/.test(inputValue)) {
+            operateObj.number1 = inputValue;
+            refreshDisplay(inputValue);
+        }
+        // keep getting left term if no operator has been inputted
+        else if (!operateObj.operator && /[0-9]/.test(inputValue)) {
+            operateObj.number1 = operateObj.number1 + inputValue;
+            refreshDisplay(operateObj.number1);
+        }
+        // get operator (first time? What if second or third time?)
+        else if (operateObj.number1 && /[\+÷×-]/.test(inputValue)) {
+            operateObj.operator = inputValue;
+            displayValue = operateObj.number1 + " " + inputValue;
+            refreshDisplay(displayValue);
+        }
+        // keep getting number2
+        else if (operateObj.number2 && /[0-9]/.test(inputValue)) {
+            operateObj.number2 = operateObj.number2 + inputValue;
+            displayValue = `${operateObj.number1} ${operateObj.operator} ${operateObj.number2}`;
+            refreshDisplay(displayValue);
+        }
+        // // if input equals second time an operator or =, then calculate result
+        // if (operateObj.number2 && /[\+÷×-]/.test(inputValue)) {
+        //     const terms = [ParseInt(operateObj.number1), ParseInt(operateObj.number2)];
+        //     operateObj.number1 = operate(operateObj.operator, terms[0], terms[1]);
+        //     refreshDisplay(operateObj.number1);
+        //     // re-initialize
+        //     delete operateObj.operator;
+        //     delete operateObj.number2;
+        // }
+        // get number 2
+        else {
+            operateObj.number2 = inputValue;
+            displayValue = `${operateObj.number1} ${operateObj.operator} ${operateObj.number2}`;
+            refreshDisplay(displayValue);
+        }
+        
     }
 ));
